fe7932dab7d88754da8d7190a74312535ad537a7
Your NameAftan <aftan.merrida@gmail.com>
2018-08-29 20:04:22 -0500

    projectwork

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-const projects = [
+let projects = [
     {
         title: "Cool Project", 
         screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
@@ -7,8 +7,55 @@ const projects = [
         available: true,
         url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
         githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-      }
+      },
+
+      {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+    } ,
+
+    {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: false,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+}
+
 
 ]
-
 console.log (projects)
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+  };
+
+  const projectsCard =() => {
+      let newprojects = '';
+      for(let i=0; i<projects.length; i++){
+          newprojects += `<div>`;
+          newprojects += `<h3> ${projects[i].title} </h3>`;
+          newprojects += `<h3> ${projects[i].screenshot} </h3>`;
+          newprojects += `<h3> ${projects[i].description} </h3>`;
+          newprojects += `<h3> ${projects[i].technologiesUsed} </h3>`;
+          newprojects += `<h3> ${projects[i].available} </h3>`;
+          newprojects += `<h3> ${projects[i].url} </h3>`;
+          newprojects += `<h3> ${projects[i].githubUrl} </h3>`;
+          if (projects[i].available === true) {
+            printToDom(newprojects, 'projectsPage');
+          }
+      
+    };
+    createProjectCards();
+
+};
+
+projectsCard();
